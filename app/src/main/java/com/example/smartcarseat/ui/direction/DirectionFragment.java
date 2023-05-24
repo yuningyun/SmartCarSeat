ced5fcdb52cc53be72a94bda1b08c7699ecd14dc
yuningyun <ing6264@naver.com>
2023-05-24 21:12:05 +0900

    direction

--- app/src/main/java/com/example/smartcarseat/ui/direction/DirectionFragment.java
+++ app/src/main/java/com/example/smartcarseat/ui/direction/DirectionFragment.java
@@ -24,8 +24,8 @@ public class DirectionFragment extends Fragment {
         binding = FragmentDirectionBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDirection;
-        directionViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+
         return root;
     }
 
